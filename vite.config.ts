9b293fe52258deee6a55221de64bdef739c089ff
mariusss11 <mariuscarchilan07@gmail.com>
2025-07-15 08:45:11 +0300

    Redeployed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/startupdev/",
+
   resolve: {
     alias: {
       "@": "/src",
